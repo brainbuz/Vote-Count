d2b349bdf86f04714e3721bdcfcb20cbe10f77a6
John Karr <brainbuz@brainbuz.org>
2019-06-30 03:10:32 -0400

    Continuing work on topcount. code for ranking choices by topcount looks like identical code will be needed for approval, so next step will be to seperate it out.

--- lib/VoteCount/TopCount.pm
+++ lib/VoteCount/TopCount.pm
@@ -4,24 +4,45 @@ use 5.026;
 
 use feature qw /postderef signatures/;
 
+package VoteCount::TopCount::Rank;
+no warnings 'experimental';
+
+# RankTopCount Method returns an object
+# The object needs to be defined in another class
+# This class within the module for another class is only
+# visble there, which is the only place it should be visible.
+
+sub new ( $class, $ordered, $byrank, $top, $bottom ) {
+  my $I = {
+    'ordered' => $ordered,
+    'byrank' => $byrank,
+    'top' => $top,
+    'bottom' => $bottom
+  };
+  return bless $I, $class;
+}
+
+sub hashwithorder ( $I ) { return $I->{'ordered'}->%* }
+sub hashbyrank ( $I ) { return $I->{'byrank'}->%* }
+sub arraytop ( $I ) { return $I->{'top'}->@* }
+sub arraybottom ( $I ) { return $I->{'bottom'}->@* }
+
+
 package VoteCount::TopCount;
 use Moose::Role;
 
 no warnings 'experimental';
+use List::Util qw( min max );
+use boolean;
 use Data::Printer;
 
 sub TopCount ( $self, $active=undef ) {
   my %ballotset = $self->ballotset()->%*;
   my %ballots = ( $ballotset{'ballots'}->%* );
-# p %ballots;
   $active = $ballotset{'choices'} unless defined $active ;
-# p $active;
   my %topcount = ( map { $_ => 0 } keys( $active->%* ));
 TOPCOUNTBALLOTS:
     for my $b ( keys %ballots ) {
-# warn "checkijng $b";
-# p $ballots{$b};
-# return {};
       my @votes = $ballots{$b}->{'votes'}->@* ;
       for my $v ( @votes ) {
         if ( defined $topcount{$v} ) {
@@ -32,5 +53,68 @@ TOPCOUNTBALLOTS:
     }
   return \%topcount;
 }
+
+sub TopCountMajority ( $self, $topcount = undef, $active = undef ) {
+  unless ( defined $topcount ) { $topcount = $self->TopCount($active) }
+  my $numvotes = 0;
+  my @choices  = keys $topcount->%*;
+  for my $t (@choices) { $numvotes += $topcount->{$t} }
+  my $thresshold = 1 + int( $numvotes / 2 );
+  for my $t (@choices) {
+    if ( $topcount->{$t} >= $thresshold ) {
+      return (
+        {
+          votes      => $numvotes,
+          thresshold => $thresshold,
+          winner     => $t,
+          winvotes   => $topcount->{$t}
+        }
+      );
+    }
+  }
+  # No winner
+  return (
+    {
+      votes      => $numvotes,
+      thresshold => $thresshold
+    }
+  );
+}
+
+sub RankTopCount ( $self, $topcount = undef, $active = undef ) {
+  unless ( defined $topcount ) { $topcount = $self->TopCount($active) }
+  my %tc      = $topcount->%*;    # destructive process needs to use a copy.
+  my %ordered = ();
+  my %byrank  = () ;
+  my $pos = 0;
+  my $maxpos = scalar( keys %tc ) ;
+  while ( 0 < scalar( keys %tc ) ) {
+    $pos++;
+    my @vtc      = values %tc;
+    my $max      = max @vtc;
+    for my $k ( keys %tc ) {
+      if ( $tc{$k} == $max ) {
+        $ordered{$k} = $pos;
+        delete $tc{ $k };
+        if ( defined $byrank{$pos} ) {
+          push @{ $byrank{$pos} }, $k;
+        }
+        else {
+          $byrank{$pos} = [ $k ];
+        }
+      }
+    }
+    die "RankTopCount in infinite loop\n" if
+      $pos > $maxpos ;
+    ;
+  }
+  # %byrank[1] is arrayref of 1st position,
+  # $pos still has last position filled, %byrank{$pos} is the last place.
+
+  return VoteCount::TopCount::Rank->new(
+    \%ordered, \%byrank, $byrank{1}, $byrank{ $pos} );
+}
+
+
 
 1;
--- t/topcount.t
+++ t/topcount.t
@@ -29,15 +29,54 @@ my $expecttc1 = {
 is_deeply( $tc1, $expecttc1,
   "Topcounted a small set with no active list as expected" );
 
-my $tc2       = $VC1->TopCount(
-    { 'VANILLA' => 1, 'CHOCOLATE' => 1, 'CARAMEL' => 1,
-      'PISTACHIO' => 0 });
+
+my $tc2 = $VC1->TopCount(
+  {
+    'VANILLA'   => 1,
+    'CHOCOLATE' => 1,
+    'CARAMEL'   => 1,
+    'PISTACHIO' => 0
+  }
+);
 my $expecttc2 = {
-  CARAMEL    => 1,
-  CHOCOLATE  => 1,
-  PISTACHIO  => 2,
-  VANILLA    => 7
-};  is_deeply( $tc2, $expecttc2,
+  CARAMEL   => 1,
+  CHOCOLATE => 1,
+  PISTACHIO => 2,
+  VANILLA   => 7
+};
+
+is_deeply( $tc2, $expecttc2,
   "Topcounted a small set with AN active list as expected" );
+
+subtest 'TopCountMajority from the same data' => sub {
+  is_deeply( $VC1->TopCountMajority( ),
+    { thresshold => 8, votes => 15 },
+    'With full ballot TopCountMajority returns only votes and thresshold');
+  is_deeply( $VC1->TopCountMajority( $tc2 ),
+    { thresshold => 6, votes => 11, winner => 'VANILLA', winvotes => 7 },
+    'Topcount from saved subset topcount TopCountMajority also gives winner info');
+};
+
+subtest 'Topcount ranking' => sub {
+  #  my @rankedtc1 = ;
+  my $x = $VC1->RankTopCount();
+  isa_ok($x, ['VoteCount::TopCount::Rank'],
+    '->RankTopCount generated object of VoteCount::TopCount::Rank');
+  can_ok( $x, [qw/ hashwithorder hashbyrank arraytop arraybottom/],
+    "have expected subs");
+  my %xwithorder = $x->hashwithorder();
+  my %xbyrank = $x->hashbyrank();
+  my @xtop = $x->arraytop();
+  my @xbottom = $x->arraybottom();
+  p %xwithorder;
+  p %xbyrank;
+  p @xtop;
+  p @xbottom;
+  # is( , 'coffi');
+
+   };
+
+
+
 
 done_testing();
