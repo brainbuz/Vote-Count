8b9f33c7ef2bffbc10dd4c9d71762ef9401a375a
John Karr <brainbuz@brainbuz.org>
2019-07-07 03:09:11 -0400

    have working matrix for condorcet.

--- lib/Vote/Count/Matrix.pm
+++ lib/Vote/Count/Matrix.pm
@@ -10,52 +10,88 @@ no warnings 'experimental';
 # use List::Util qw( min max );
 # use Vote::Count::RankCount;
 # use Try::Tiny;
-# use boolean;
 use Data::Printer;
 
 has BallotSet => (
+  is => 'ro',
+  required => 1,
+  isa => 'HashRef',
+);
 
-)
+has BallotSetType => (
+  is => 'ro',
+  isa => 'Str',
+  default => 'rcv',
+);
 
-# has TieBreakTable => (
-#   is => 'ro',
-#   isa => 'Object',
-#   lazy => 1,
-#   builder => '_buildtiebreaktable',
-# );
+has Active => (
+  is => 'rw',
+  isa => 'ArrayRef',
+  builder => 'Vote::Count::Matrix::_buildActive',
+  lazy => 1,
+);
 
-# sub _buildtiebreaktable ( $self ) {
-#   my ( $table, $discard ) = $self->Boorda();
-#   return $table;
-# }
-# has 'bordaweight' => (
-#   is => 'rw',
-#   isa => 'CodeRef',
-#   builder => '_buildbordaweight',
-#   lazy => 1,
-# );
+sub _buildActive ( $self ) {
+  return [ keys $self->BallotSet->{'choices'}->%* ] ;
+}
 
-# has 'bordadepth' => (
-#   is => 'rw',
-#   isa => 'Int',
-#   default => 0,
-# );
+sub _conduct_pair ( $ballotset, $A, $B ) {
+  my $ballots = $ballotset->{'ballots'};
+  my $countA  = 0;
+  my $countB  = 0;
+FORVOTES:
+  for my $b ( keys $ballots->%* ) {
+    for my $v ( values $ballots->{$b}{'votes'}->@* ) {
+      if    ( $v eq $A ) {
+        $countA += $ballots->{$b}{'count'};
+        next FORVOTES }
+      elsif ( $v eq $B ) {
+        $countB += $ballots->{$b}{'count'};
+        next FORVOTES }
+    }
+  } # FORVOTES
+  my %retval = (
+    $A        => $countA,
+    $B        => $countB,
+    'tie'     => 0,
+    'winner'  => '',
+    'loser'   => '',
+    'margin'  => abs( $countA - $countB )
+    );
+  if ( $countA == $countB ) {
 
-=pod
+    # tiebreak will happen here.
+    $retval{'winner'} = '';
+    $retval{'tie'}    = 1;
+  }
+  elsif ( $countA > $countB ) {
+    $retval{'winner'} = $A;
+    $retval{'loser'}  = $B;
+  }
+  elsif ( $countB > $countA ) {
+    $retval{'winner'} = $B;
+    $retval{'loser'}  = $A;
+  }
+  return \%retval;
+}
 
-=head2 Populate
-
-Populate Matrix for Pairwise methods.
-
-=cut
-
-sub Populate (
-  $self,
-  $active=[ keys $self->ballotset()->{'choices'}->%* ]
-  ) {
-  my $Matrix = {};
-p $active;
-  $self->{'Matrix'} = $Matrix;
+sub BUILD  {
+  my $self = shift;
+  my $results = {};
+  my $ballotset = $self->BallotSet();
+  my @choices =  @{$self->Active} ;
+  while ( scalar(@choices )) {
+    my $A = shift @choices;
+    for my $B ( @choices ) {
+      my $result = Vote::Count::Matrix::_conduct_pair(
+        $ballotset , $A, $B );
+      # Each result has two hash keys so it can be found without
+      # having to try twice or sort the names for a single key.
+      $results->{$A}{$B} = $result;
+      $results->{$B}{$A} = $result;
+    }
+  }
+  $self->{'Matrix'} = $results;
 }
 
 1;
--- t/matrix.t
+++ t/matrix.t
@@ -13,20 +13,71 @@ use feature qw /postderef signatures/;
 use Path::Tiny;
 
 use Vote::Count;
+use Vote::Count::Matrix;
 use Vote::Count::ReadBallots 'read_ballots';
 
-my $M1 = Vote::Count->new(
-  ballotset  => read_ballots('t/data/data1.txt'),
+my $M1 = Vote::Count::Matrix->new(
+  'BallotSet' => read_ballots('t/data/ties1.txt'),
 );
 
-can_ok( $M1,
-  [qw/Populate/],
-  "expected subs for a matrix");
+# can_ok( $M1,
+#   [qw/Populate/],
+#   "expected subs for a matrix");
+isa_ok( $M1, ['Vote::Count::Matrix'],
+'The matrix is a Vote::Count::Matrix');
 
+subtest '_conduct_pair returns hash with pairing info' => sub {
+  my $t1 = Vote::Count::Matrix::_conduct_pair(
+    $M1->BallotSet, 'RUMRAISIN', 'STRAWBERRY');
+  my $x1 = {
+    loser    =>   "",
+    margin   =>  0,
+    RUMRAISIN  => 4,
+    STRAWBERRY => 4,
+    tie    => 1,
+    winner=> "",
+};
+  is_deeply( $t1, $x1, 'A Tie');
+  my $t2 = Vote::Count::Matrix::_conduct_pair(
+    $M1->BallotSet, 'RUMRAISIN', 'FUDGESWIRL');
+  my $x2 = {
+    FUDGESWIRL => 6,
+    loser => "RUMRAISIN",
+    margin => 2,
+    RUMRAISIN => 4,
+    tie => 0,
+    winner => "FUDGESWIRL",
+};
+  is_deeply( $t2, $x2, 'has winner');
+};
 
-# p $M1->TieBreakTable();
+subtest 'check some in the matrix' => sub {
+  my $xVanMint = {
+    loser    => "",
+    margin   => 0,
+    MINTCHIP => 6,
+    tie      => 1,
+    VANILLA  => 6,
+    winner   => ""
+  };
+  my $xRockStraw = {
+    loser      => "STRAWBERRY",
+    margin     => 1,
+    ROCKYROAD  => 5,
+    STRAWBERRY => 4,
+    tie        => 0,
+    winner     => "ROCKYROAD"
+  };
+  my $VanMint = $M1->{'Matrix'}{'VANILLA'}{'MINTCHIP'};
+  is_deeply( $xVanMint, $VanMint, 'check a tie');
+  my $RockStraw = $M1->{'Matrix'}{'ROCKYROAD'}{'STRAWBERRY'};
+  is_deeply( $xRockStraw, $RockStraw, 'one with a winner');
+  is_deeply(
+    $M1->{'Matrix'}{'FUDGESWIRL'}{'CHOCCHUNK'},
+    $M1->{'Matrix'}{'CHOCCHUNK'}{'FUDGESWIRL'},
+    'access a result in both possible pairing orders identical'
+  );
 
-$M1->Populate();
-# p $M1->{'Matrix'};
+};
 
 done_testing();
